a13f8b26de89b900a1dd421bbc3d27523087170d
Regis <boudinot.regis@yahoo.com>
2016-05-09 20:42:30 -0600

    reegbase alpha - will add all methods later

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
 const http = require('http')
 const express = require('express')
 const socketIo = require('socket.io')
-const _ = require('lodash')
 const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
 const port = process.env.PORT || 3000
 const Selfup = require('selfup-rejs')
 const rejs = new Selfup
@@ -18,34 +15,21 @@ const server = http.createServer(app)
 
 const io = socketIo(server)
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/getTable/:tableName', (req, res) => {
-  res.json(rejs.getTable(`${req.params.tableName}`))
-})
-
-app.get('/createTable/:tableName', (req, res) => {
-  let tableName = req.params.tableName
-  rejs.createTable(`${tableName}`)
-  res.json({createdTable: tableName})
-})
-
-app.get('/dropTable/:tableName', (req, res) => {
-  let tableName = req.params.tableName
-  rejs.dropTable(`${tableName}`)
-  res.json({droppedTable: tableName})
-})
-
-app.post('/newData/:tableName', (req, res) => {
-  let tableName = req.params.tableName
-  rejs.newData(tableName, req.body)
-  res.json({status: 201})
-})
-
 io.sockets.on('connection', socket => {
   socket.on('message', (channel, message) => {
-    // Sockets will be implemented here
+    if (channel === 'createTable') {
+      rejs.createTable(`${message}`)
+    }
+    if (channel === 'newData') {
+      rejs.newData(`${message[0]}`, message[1])
+    }
+    if (channel === 'getTable') {
+      let table = rejs.getTable(`${message}`)
+      io.to(socket.id).emit('sendTable', table)
+    }
+    if (channel === 'dropTable') {
+      rejs.dropTable(`${message}`)
+    }
   })
 })
 
